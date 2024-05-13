f8f3d019f0d3c6dcd1c887d5098a3bc3b0238a0b
abiyah-olazabal <abiyah.olazabalc@gmail.com>
2024-05-12 23:01:52 -0500

    report download.py

--- report download.py
+++ report download.py
@@ -3,7 +3,7 @@ from openpyxl import Workbook
 from openpyxl import load_workbook
 from bs4 import BeautifulSoup
 import pandas as pd
-filesheet = r"C:\Users\akolazabalc.CD-ROSAS\Documents\Cancelaciones\2024\Historial\Envios de ws 2024 - Marzo.xlsx"
+filesheet = "Envios de ws 2024 - Marzo.xlsx"
 df=pd.read_excel(filesheet, sheet_name='SRX', usecols=['NFOLIO','ESTADO'])
 df_folios = df.loc[(df['ESTADO'] == 'Digitado')|(df['ESTADO'] == 'En Ruta'), 'NFOLIO']
 texto = "','".join(df_folios.astype(str))
@@ -35,7 +35,7 @@ if response.status_code == 200:
             print(fila_excel)
             hoja_excel.append(fila_excel)
         # Guardar el libro de Excel
-        libro_excel.save(r'C:\Users\akolazabalc.CD-ROSAS\Documents\Cancelaciones\2024\informacion.xlsx')
+        libro_excel.save('informacion.xlsx')
         #'''
         #print(tabla)
 else:
